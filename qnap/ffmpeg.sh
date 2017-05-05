a3b92d8a6ba6faed3298fe9deaa882c9f6d74cf0
bugfire <bugfire@users.noreply.github.com>
2017-05-06 02:34:50 +0900

    updated

--- qnap/ffmpeg.sh
+++ qnap/ffmpeg.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 #ffmpeg=ffmpeg
 #ffmpeg=/mnt/ext/opt/medialibrary/bin/ffmpeg
 ffmpeg=/opt/bin/ffmpeg
 
-for i in `find . -name "*.[Mm][Tt][Ss]" | grep -v thumb | grep -v "^\."`; do
+for i in `find . -name "*.[Mm][Tt][Ss]" | grep -v thumb | grep -v "/\."`; do
   j=`basename $i .mts`
   j=`basename $j .MTS`
   k=`dirname $i`
@@ -13,6 +13,6 @@ for i in `find . -name "*.[Mm][Tt][Ss]" | grep -v thumb | grep -v "^\."`; do
     continue;
   fi
   echo $i to $n...
-  # ffmpeg -y -i $i -f mp4 -vcodec copy -acodec libvo_aacenc -ab 192k -ar 44100 -ac 2 $n
-  $ffmpeg -n -i $i -f mp4 -vcodec libx264 -profile:v baseline -aspect 16:9 -preset:v superfast -level:v 3.2 -acodec ac3 -ac 2 $n
+  $ffmpeg -y -i $i -f mp4 -vcodec libx264 -profile:v baseline -preset:v superfast -level:v 3.2 -acodec ac3 -ac 2 $n.tmp
+  mv $n.tmp $n
 done

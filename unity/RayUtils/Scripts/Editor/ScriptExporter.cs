304bd0babd1b19f95fb56904f2936c93f25ebc60
bugfire <bugfire@users.noreply.github.com>
2017-05-16 07:50:06 +0900

    Added RayProfiler.unitypackage

---
+++ unity/RayUtils/Scripts/Editor/ScriptExporter.cs
@@ -0,0 +1,29 @@
+﻿// RayUtils
+// https://github.com/Bugfire
+
+using UnityEditor;
+
+namespace RayStorm
+{
+	
+	static class ScriptExporter
+	{
+
+		#region menu item
+
+		[MenuItem ("Plugin/Management/ExportRayProfiler")]
+		static void ExportRayProfiler ()
+		{
+			string _MyPackage = "Assets/Plugin/RayProfiler.unitypackage";
+			string[] _MyAssets = new string[] {
+				"Assets/Plugin/RayCanvas",
+				"Assets/Plugin/RayProfiler",
+				"Assets/Plugin/RayUtils",
+			};
+			AssetDatabase.ExportPackage (_MyAssets, _MyPackage, ExportPackageOptions.Recurse);
+		}
+
+		#endregion
+
+	}
+}
